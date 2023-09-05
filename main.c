7eb3583f86f26206b593ba482f6c543138360076
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2023-09-05 15:42:28 +0100

    simplification
    The TVA is now calculated in the conditions

--- main.c
+++ main.c
@@ -4,18 +4,16 @@ int main() {
     float applePrice = 3;
     float sodaPrice = 7;
     float cakePrice = 1;
-    double selectedPrice = 0;
+    double selectedPrice = 1.2;
     char selectedProduct;
 
     printf("Quel produit souhaitez vous acheter (\"p\" pour pomme, \"s\" pour soda ou \"g\" pour gâteau) :\n");
     scanf("%c", &selectedProduct);
 
-    if(selectedProduct == 'p') selectedPrice = applePrice;
-    else if(selectedProduct == 's') selectedPrice = sodaPrice;
-    else if(selectedProduct == 'g') selectedPrice = cakePrice;
+    if(selectedProduct == 'p') selectedPrice *= applePrice;
+    else if(selectedProduct == 's') selectedPrice *= sodaPrice;
+    else if(selectedProduct == 'g') selectedPrice *= cakePrice;
     else { printf("Erreur produit invalide !"); return 0;}
-
-    selectedPrice*=1.20;
 
     printf("Vous devez payer %lf¤", selectedPrice);
 
